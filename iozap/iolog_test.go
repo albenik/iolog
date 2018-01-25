b085b1dbfe7e6ce25c336ea383b28564a411329b
Veniamin Albaev <albenik@gmail.com>
2018-01-25 17:11:41 +0300

    Fixed tests to use UTC time instead of Local time

--- iozap/iolog_test.go
+++ iozap/iolog_test.go
@@ -15,68 +15,75 @@ import (
 	"github.com/albenik/iolog/iozap"
 )
 
-type testConfig struct {
-	data  []*iolog.Record
-	regex *regexp.Regexp
-}
-
 func TestLog(t *testing.T) {
-	cfg := []testConfig{
+	testdata := []struct {
+		data  []*iolog.Record
+		regex *regexp.Regexp
+		msg   string
+	}{
 		{
 			data: []*iolog.Record{
 				{
 					Operation: iolog.Read,
-					Start:     time.Date(2018, 1, 1, 12, 30, 50, 0, time.Local),
-					Stop:      time.Date(2018, 1, 1, 12, 31, 01, 22, time.Local),
+					Start:     time.Date(2018, 1, 1, 12, 30, 01, 11111, time.UTC),
+					Stop:      time.Date(2018, 1, 1, 12, 31, 02, 22222, time.UTC),
 					Data:      []byte{1, 2, 3, 4, 5},
 					Error:     nil,
 				},
 			},
-			regex: regexp.MustCompile(`{"level":"info","ts":\d+\.\d+,"msg":"Test ok","iolog":\[{"op":"read","start":1514799050,"stop":1514799061,"dur":11.000000022,"data":"01 02 03 04 05","error":""}]}`),
+			regex: regexp.MustCompile(`{"level":"info","ts":\d+\.\d+,"msg":"Test ok","iolog":\[{"op":"read","start":1514809801.000011,"stop":1514809862.0000222,"dur":61.000011111,"data":"01 02 03 04 05","error":""}]}`),
+			msg:   "log 1",
 		}, {
 			data: []*iolog.Record{
 				{
 					Operation: iolog.Write,
-					Start:     time.Date(2018, 1, 1, 12, 30, 50, 0, time.Local),
-					Stop:      time.Date(2018, 1, 1, 12, 31, 01, 22, time.Local),
+					Start:     time.Date(2018, 1, 1, 12, 30, 01, 11111, time.UTC),
+					Stop:      time.Date(2018, 1, 1, 12, 31, 02, 22222, time.UTC),
 					Data:      []byte{1, 2, 3, 4, 5},
 					Error:     nil,
 				},
 			},
-			regex: regexp.MustCompile(`{"level":"info","ts":\d+\.\d+,"msg":"Test ok","iolog":\[{"op":"write","start":1514799050,"stop":1514799061,"dur":11.000000022,"data":"01 02 03 04 05","error":""}]}`),
+			regex: regexp.MustCompile(`{"level":"info","ts":\d+\.\d+,"msg":"Test ok","iolog":\[{"op":"write","start":1514809801.000011,"stop":1514809862.0000222,"dur":61.000011111,"data":"01 02 03 04 05","error":""}]}`),
+			msg:   "log 2",
 		}, {
 			data: []*iolog.Record{
 				{
 					Operation: iolog.Close,
-					Start:     time.Date(2018, 1, 1, 12, 30, 50, 0, time.Local),
-					Stop:      time.Date(2018, 1, 1, 12, 30, 50, 5, time.Local),
+					Start:     time.Date(2018, 1, 1, 12, 30, 50, 11111, time.UTC),
+					Stop:      time.Date(2018, 1, 1, 12, 30, 50, 22222, time.UTC),
 					Data:      nil,
 					Error:     nil,
 				},
 			},
-			regex: regexp.MustCompile(`{"level":"info","ts":\d+\.\d+,"msg":"Test ok","iolog":\[{"op":"close","start":1514799050,"stop":1514799050,"dur":0.000000005,"data":"","error":""}]}`),
+			regex: regexp.MustCompile(`{"level":"info","ts":\d+\.\d+,"msg":"Test ok","iolog":\[{"op":"close","start":1514809850.000011,"stop":1514809850.0000222,"dur":0.000011111,"data":"","error":""}]}`),
+			msg:   "log 3",
 		},
 		{
 			data: []*iolog.Record{
 				{
 					Operation: iolog.Read,
-					Start:     time.Date(2018, 1, 1, 12, 30, 50, 0, time.Local),
-					Stop:      time.Date(2018, 1, 1, 12, 30, 50, 0, time.Local),
+					Start:     time.Date(2018, 1, 1, 12, 30, 50, 11111, time.UTC),
+					Stop:      time.Date(2018, 1, 1, 12, 30, 50, 11111, time.UTC),
 					Data:      []byte{9},
 					Error:     io.EOF,
 				},
 			},
-			regex: regexp.MustCompile(`{"level":"info","ts":\d+\.\d+,"msg":"Test ok","iolog":\[{"op":"read","start":1514799050,"stop":1514799050,"dur":0,"data":"09","error":"EOF"}]}`),
+			regex: regexp.MustCompile(`{"level":"info","ts":\d+\.\d+,"msg":"Test ok","iolog":\[{"op":"read","start":1514809850.000011,"stop":1514809850.000011,"dur":0,"data":"09","error":"EOF"}]}`),
+			msg:   "log 4",
 		},
 	}
 	out := new(bytes.Buffer)
-	core := zapcore.NewCore(zapcore.NewJSONEncoder(zap.NewProductionEncoderConfig()), zapcore.Lock(zapcore.AddSync(out)), zap.DebugLevel)
+	core := zapcore.NewCore(
+		zapcore.NewJSONEncoder(zap.NewProductionEncoderConfig()),
+		zapcore.Lock(zapcore.AddSync(out)),
+		zap.DebugLevel,
+	)
 	logger := zap.New(core)
 
-	for _, c := range cfg {
+	for _, tt := range testdata {
 		out.Reset()
-		logger.Info("Test ok", zap.Array("iolog", iozap.Log(c.data)))
+		logger.Info("Test ok", zap.Array("iolog", iozap.Log(tt.data)))
 		logger.Sync()
-		assert.Regexp(t, c.regex, out.String())
+		assert.Regexp(t, tt.regex, out.String(), tt.msg)
 	}
 }
