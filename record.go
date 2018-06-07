0a116160b9cd6bd25b499607a7351d8c4083b7e1
Veniamin Albaev <albenik@gmail.com>
2018-06-08 01:14:41 +0300

    Panic condition fixed

--- record.go
+++ record.go
@@ -61,17 +61,21 @@ func (r *Record) String() string {
 	if stop.IsZero() {
 		stop = r.Start
 	}
-	var data string
+	var datastr string
 	if r.Data != nil { // if not any typed value (but typed <nil> allowed)
 		switch d := r.Data.(type) {
 		case []byte:
-			data = fmt.Sprintf("[% X]", d)
+			datastr = fmt.Sprintf("[% X]", d)
 		case fmt.Stringer:
-			data = d.String()
+			if d == nil {
+				datastr = "nil"
+			} else {
+				datastr = d.String()
+			}
 		default:
-			data = fmt.Sprintf("%+v", d)
+			datastr = fmt.Sprintf("%+v", d)
 		}
 	}
 	const tf = "2006-01-02T15:04:05.000-0700"
-	return fmt.Sprintf("%s %s (%s) %s / %s error: %v", r.Tag, data, stop.Sub(r.Start), r.Start.Format(tf), r.Start.Format(tf), r.Error)
+	return fmt.Sprintf("%s %s (%s) %s / %s error: %v", r.Tag, datastr, stop.Sub(r.Start), r.Start.Format(tf), r.Start.Format(tf), r.Error)
 }
